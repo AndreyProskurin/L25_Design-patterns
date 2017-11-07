c6844011fa68eb2a7a1189020ed64f7f0093d946
Andrey Proskurin <andrewproskurin@gmail.com>
2017-11-07 19:46:04 +0200

    Empty single view app has been added.

---
+++ L25_Design-patterns/L25_Design-patterns/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  L25_Design-patterns
+//
+//  Created by Andrey Proskurin on 07.11.17.
+//  Copyright © 2017 Andrey Proskurin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ L25_Design-patterns/L25_Design-patterns/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  L25_Design-patterns
+//
+//  Created by Andrey Proskurin on 07.11.17.
+//  Copyright © 2017 Andrey Proskurin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
